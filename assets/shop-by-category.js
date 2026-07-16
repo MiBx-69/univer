57ee1c0ade22ea142268bcb83c2fe39f855052df
Moinul Islam Bappi <moinul@mibrand.agency>
2026-07-16 11:50:05 +0600

    new catgories

---
+++ assets/shop-by-category.js
@@ -0,0 +1,6 @@
+// shop-by-category.js
+document.addEventListener('DOMContentLoaded',()=>{
+  document.querySelectorAll('.shop-category__track').forEach(track=>{
+    // Add custom carousel logic here.
+  });
+});
